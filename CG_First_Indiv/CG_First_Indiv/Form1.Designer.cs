845836ea6297ca93eadca2ebff458d4cfbc29de4
MyPleasure <kir_zinn@mail.ru>
2023-11-03 23:59:41 +0300

    first_steps

--- CG_First_Indiv/CG_First_Indiv/Form1.Designer.cs
+++ CG_First_Indiv/CG_First_Indiv/Form1.Designer.cs
@@ -28,13 +28,182 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(1125, 32);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(162, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1063, 727);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.DrawPoint);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1158, 709);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 30);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(1186, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Point";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(1124, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(1258, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(1090, 115);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "X";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(1090, 161);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(20, 20);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Y";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(1148, 161);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 12;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar1.Scroll += new System.EventHandler(this.Scroll_Y_Bar);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(1148, 115);
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(104, 45);
+            this.trackBar2.TabIndex = 13;
+            this.trackBar2.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar2.Scroll += new System.EventHandler(this.Scroll_X_Bar);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(1124, 161);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(18, 20);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(1258, 161);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(18, 20);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1343, 751);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
     }
 }
 
--- CG_First_Indiv/CG_First_Indiv/Form1.cs
+++ CG_First_Indiv/CG_First_Indiv/Form1.cs
@@ -7,14 +7,209 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace CG_First_Indiv
 {
+
+    public class Triangle 
+    {
+        public Point fpoint;
+        public Point spoint;
+        public Point tpoint;
+
+        public Triangle(Point fpoint, Point spoint, Point tpoint)
+        {
+            this.fpoint = fpoint;
+            this.spoint = spoint;
+            this.tpoint = tpoint;
+        }
+
+        public Triangle() { }
+    }
+
     public partial class Form1 : Form
     {
+
+        private Point point = new Point(0, 0);
+
+        private Point CurPoint = Point.Empty;
+
+        private int CurPointInd = 0;
+
+        private int CountPoints = 0;
+
+        private List<Point> points = new List<Point>();
+
+        private SolidBrush brush = new SolidBrush(Color.Red);
+
+        private Graphics g;
+
+        private Triangle mTriangle = new Triangle();
+
         public Form1()
         {
             InitializeComponent();
+            g = pictureBox1.CreateGraphics();
+
+            //X
+            trackBar1.Minimum = 0;
+            trackBar1.Maximum = pictureBox1.Height;
+            trackBar1.TickFrequency = 1;
+            label7.Text = pictureBox1.Height.ToString();
+
+            //Y
+            trackBar2.Minimum = 0;
+            trackBar2.Maximum = pictureBox1.Width;
+            trackBar2.TickFrequency = 1;
+            label3.Text = pictureBox1.Width.ToString();
+        }
+
+        private void DrawPoint(object sender, MouseEventArgs e)
+        {
+            point = e.Location;
+            if (!points.Contains(point))
+                points.Add(point);
+            if (points.Count >= 4) 
+            {
+                mTriangle = Calculate_Main_Triangle();
+            }
+            comboBox1.Items.Add($"Point {CountPoints}");
+            CountPoints++;
+            pictureBox1.Invalidate();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (Point p in points) 
+            {
+                e.Graphics.FillEllipse(brush, p.X - 3, p.Y - 3, 3, 3);
+                Console.WriteLine(p.X);
+            }
+
+            if (points.Count() >= 4) 
+            {
+                Point[] tp = { mTriangle.fpoint, mTriangle.tpoint, mTriangle.spoint };
+                e.Graphics.DrawPolygon(new Pen(Color.Black), tp);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            point = Point.Empty;
+            mTriangle = new Triangle();
+            label1.Text = "Point";
+            CurPoint = Point.Empty;
+            points.Clear();
+            comboBox1.Items.Clear();
+            comboBox1.Text = "";
+            trackBar1.Value = 0;
+            trackBar2.Value = 0;
+            CountPoints = 0;
+            g.Clear(Color.White);
+        }
+
+        private void Scroll_X_Bar(object sender, EventArgs e)
+        {
+            if (CurPoint != Point.Empty) 
+            {
+                CurPoint.X = trackBar2.Value;
+                points[comboBox1.SelectedIndex] = CurPoint;
+                pictureBox1.Invalidate();
+            }
+
+            if (points.Count >= 4)
+            {
+                mTriangle = Calculate_Main_Triangle();
+            }
+        }
+
+        private void Scroll_Y_Bar(object sender, EventArgs e)
+        {
+            if (CurPoint != Point.Empty)
+            {
+                CurPoint.Y = trackBar1.Value;
+                points[comboBox1.SelectedIndex] = CurPoint;
+                pictureBox1.Invalidate();
+            }
+
+            if (points.Count >= 4)
+            {
+                mTriangle = Calculate_Main_Triangle();
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            CurPoint = points[comboBox1.SelectedIndex];
+            label1.Text = $"Point {comboBox1.SelectedIndex}";
+            trackBar2.Value = points[comboBox1.SelectedIndex].X;
+            trackBar1.Value = points[comboBox1.SelectedIndex].Y;
+        }
+
+
+        private Triangle Calculate_Main_Triangle() 
+        {
+            int minx = int.MaxValue; 
+            int miny = int.MaxValue;
+            int maxx = int.MinValue;
+            int maxy = int.MinValue;
+            foreach (Point p in points) 
+            {
+                if (p.X < minx)
+                    minx = p.X;
+                if (p.X > maxx)
+                    maxx = p.X;
+                if (p.Y < miny)
+                    miny = p.Y;
+                if (p.Y > maxy)
+                    maxy = p.Y;
+            }
+
+            var x1 = minx + 10;
+            var y1 = miny - 10;
+
+            var x2 = (minx + maxx) / 2;
+            var y2 = maxy + 10;
+
+            var x3 = maxx + 10;
+            var y3 = miny - 10;
+
+            var k1 = (y2 - y1) / (x2 - x1);
+            var l1 = -1 * ((x1 * y2 + x2 * y1) / (x2 - x1));
+
+            var k2 = (y2 - y3) / (x2 - x3);
+            var l2 = -1 * ((x3 * y2 + x2 * y3) / (x2 - x3));
+
+            var k3 = (y3 - y1 / (x3 - x1));
+            var l3 = -1 * ((x1 * y3 + x3 * y1) / (x3 - x1));
+
+            bool flag = false;
+            while (!flag) 
+            {
+                foreach (Point p in points) 
+                {
+                    if ((k1 * x2 + l1) > p.Y && (k2 * x2 + l2) > p.Y && (k3 * x1 + l3) < p.Y)
+                    {
+                        flag = true;
+                    }
+                    else
+                        flag = false;
+                }
+                x1--;
+                y2++;
+                x3++;
+                k1 = (y2 - y1) / (x2 - x1);
+                l1 = -1 * ((x1 * y2 + x2 * y1) / (x2 - x1));
+
+                k2 = (y2 - y3) / (x2 - x3);
+                l2 = -1 * ((x3 * y2 + x2 * y3) / (x2 - x3));
+
+                k3 = (y3 - y1 / (x3 - x1));
+                l3 = -1 * ((x1 * y3 + x3 * y1) / (x3 - x1));
+            }
+
+            return new Triangle(new Point(x1, y1), new Point(x2, y2), new Point(x3, y3));
         }
     }
 }
